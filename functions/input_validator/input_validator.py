2078bdbb9c3f3d82fe21c389ca12c60eccd08db7
Harbinger67373 <gwanders380@gmail.com>
2023-03-04 10:16:06 -0500

    Removed unecessary infrastructure SAM file.

--- functions/input_validator/input_validator.py
+++ functions/input_validator/input_validator.py
@@ -1,7 +1,9 @@
 import json
+import pandas as pd
 
 def lambda_handler(event, context):
     input = event['body']
+    df = pd.DataFrame(data=None)
     return {
         'statusCode': 200,
         'body': json.dumps(input)
