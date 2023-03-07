aeba6a95606ec91d1282bc35ffdc390cdb7771b1
Harbinger67373 <gwanders380@gmail.com>
2023-03-07 17:21:43 -0500

    Refactored Lambda Handler

--- functions/input_validator/input_validator.py
+++ functions/input_validator/input_validator.py
@@ -1,10 +1,18 @@
 import json
 import boto3
 
+http_success_response_code = 200
+
 
 def lambda_handler(event, context):
-    input = event['body']
     return {
         'statusCode': 200,
-        'body': json.dumps(input)
+        'body': construct_return_json(http_success_response_code, event['body'])
+    }
+
+
+def construct_return_json(status, message):
+    return {
+        'statusCode': status,
+        'body': json.dumps(message)
     }
