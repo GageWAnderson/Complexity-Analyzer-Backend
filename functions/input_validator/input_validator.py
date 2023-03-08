51a8e3ac84d99277bf43c6b141e6fb259310be2c
Harbinger67373 <gwanders380@gmail.com>
2023-03-08 13:56:33 -0500

    Added substantial validation logic to input_validator. Commented out calls to code security analyzer and code complexity analyzer functions until those lambdas are created.

--- functions/input_validator/input_validator.py
+++ functions/input_validator/input_validator.py
@@ -1,18 +1,82 @@
 import json
+from requests import codes
 import boto3
 
-http_success_response_code = 200
+max_code_length = 500
+
+min_args_number = 1
+max_args_number = 3
+
+client = boto3.client('lambda')
 
 
 def lambda_handler(event, context):
-    return {
-        'statusCode': 200,
-        'body': construct_return_json(http_success_response_code, event['body'])
-    }
+    headers = event['headers']
+
+    if 'Content-Type' not in headers or headers['Content-Type'] != 'application/json':
+        return {
+            'statusCode': codes.bad_request,
+            'body': json.dumps({'message': 'Invalid Content-Type header'})
+        }
+
+    body = event['body']
+
+    if not body:
+        return construct_response(codes.bad_request, 'Empty request body')
+
+    try:
+        body_json = json.loads(body)
+    except json.JSONDecodeError as e:
+        return construct_response(codes.bad_request, 'Invalid JSON body', str(e))
+
+    # Your validation logic here
+    if 'inputCode' not in body_json:
+        return construct_response(codes.bad_request, 'Missing input code field')
+    elif 'inputCode' in body_json and len(body_json['inputCode']) > max_code_length:
+        return construct_response(codes.bad_request, 'Input code too long')
+    elif 'args' not in body_json:
+        return construct_response(codes.bad_request, 'Missing arguments field')
+    elif 'args' in body_json and len(body_json['args']) > max_args_number or len(body_json['args']) < min_args_number:
+        return construct_response(codes.bad_request, 'Invalid argument number')
+    else:
+        for arg in body_json['args']:
+            response = validate_argument(arg)
+            if response:
+                return response
+
+    if validate_code_security(body_json['inputCode']):
+        # Call Code Complexity Analyzer Lambda Function
+        return construct_response(codes.ok, 'Request validated successfully')
+    else:
+        return construct_response(codes.bad_request, 'Input code failed security check, warning: this may be a malicious request')
+
+def validate_code_security(code):
+    return True
+    # response = client.invoke(
+    #     FunctionName='code_security_validator',
+    #     InvocationType='RequestResponse',
+    #     Payload=json.dumps({'inputCode': code})
+    # )
+
+    # if response['StatusCode'] == codes.ok:
+    #     response_body = json.loads(response['Payload'].read())
+    #     return response_body['message'] == 'Code security check passed'
+    # else:
+    #     return False
+
+def validate_argument(argJsonObject):
+    if 'argName' not in argJsonObject:
+        return construct_response(codes.bad_request, 'Missing argument name field')
+    elif 'argType' not in argJsonObject:
+        return construct_response(codes.bad_request, 'Missing argument type field')
+    elif 'variable' not in argJsonObject:
+        return construct_response(codes.bad_request, 'Missing variable field')
+    else:
+        return None
 
 
-def construct_return_json(status, message):
+def construct_response(status_code, message, error=None):
     return {
-        'statusCode': status,
-        'body': json.dumps(message)
+        'statusCode': status_code,
+        'body': json.dumps({'message': message, 'error': error})
     }
