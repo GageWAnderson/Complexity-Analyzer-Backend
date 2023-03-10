11ac4d10052b7c0a4c06f4381397f0dc67c6cb7c
Harbinger67373 <gwanders380@gmail.com>
2023-03-10 18:02:39 -0500

    Added additional formatting code for writing user results to S3 bucket.

--- functions/post_complexity_analyzer_results/post_complexity_analyzer_results.py
+++ functions/post_complexity_analyzer_results/post_complexity_analyzer_results.py
@@ -2,11 +2,14 @@ import json
 import boto3
 from requests import codes
 import logging
+import time
 
-s3client = boto3.client('s3')
+s3 = boto3.resource('s3')
+bucket_name = 'code-complexity-analyzer-results-test'
 
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
+
 
 def lambda_handler(event, context):
 
@@ -15,7 +18,8 @@ def lambda_handler(event, context):
         args = event['args']
         complexity = event['complexity']
         user_id = event['user-id']
-        logger.debug(f'Input code: {inputCode}, args: {args}, complexity: {complexity}, user-id: {user_id}')
+        logger.debug(
+            f'Input code: {inputCode}, args: {args}, complexity: {complexity}, user-id: {user_id}')
         post_to_s3(inputCode, args, complexity, user_id)
     except Exception as e:
         return {
@@ -23,14 +27,23 @@ def lambda_handler(event, context):
             'body': json.dumps({'message': 'Bad request'})
         }
 
+
 def post_to_s3(inputCode, args, complexity, user_id):
-    s3client.put_object(
-        Bucket='code-complexity-analyzer-results',
-        Key='results.json',
-        Body=json.dumps({
-            'inputCode': inputCode,
-            'args': args,
-            'complexity': complexity,
-            'user-id': user_id
-        })
-    )
+    bucket = s3.Bucket(bucket_name)
+    prefix_objs = list(bucket.objects.filter(Prefix=f'{user_id}/'))
+    prefix_exists = len(prefix_objs) > 0
+
+    if not prefix_exists:
+        bucket.put_object(Key=f'{user_id}/')
+
+    timestamp = str(int(time.time()))
+    object_key = f'{user_id}/{timestamp}.json'
+
+    s3_object = s3.Object(bucket_name, object_key)
+    logger.debug(f'Writing to s3 object: {s3_object}')
+    s3_object.put(Body=json.dumps({
+        'inputCode': inputCode,
+        'args': args,
+        'complexity': complexity,
+        'user-id': user_id
+    }))
